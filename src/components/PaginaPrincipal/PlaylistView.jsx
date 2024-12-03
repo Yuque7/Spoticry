0787968defd7f893eaac6ff395fd2b9e05d48e7a
HianMaths <hianmatheusofc1@gmail.com>
2024-12-02 23:58:18 -0300

    Adicionando PlaylistView da pasta PaginaPrincipal

---
+++ src/components/PaginaPrincipal/PlaylistView.jsx
@@ -0,0 +1,201 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate, useLocation } from "react-router-dom"; 
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { HiOutlineArrowSmLeft } from "react-icons/hi";
+import { GlobalStyled } from "../../GlobalStyled";
+import { deletePlaylist } from "../../services/playlist"; 
+ // Importando o componente de edição
+
+import {
+  Container,
+  PlaylistHeader,
+  PlaylistInfo,
+  PlaylistImage,
+  PlaylistDetails,
+  LeaveButton,
+  PlaylistTable,
+  Thumbnail,
+  DefaultImage,
+  SpinnerContainer,
+  Spinner,
+  ButtonDelete
+} from "../../styles/PaginaPrincipal/PlaylistViewStyled";
+import EditPlaylistComponent from "./EditPlaylist";
+
+function PlaylistView() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [playlist, setPlaylist] = useState(null);
+  const [music, setMusic] = useState([]);
+  const [isCreator, setIsCreator] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [newSongId, setNewSongId] = useState("");
+  const [newPlaylistName, setNewPlaylistName] = useState("");  // Estado para o novo nome da playlist
+  const [isModalOpen, setModalOpen] = useState(false);  // Estado para controlar a abertura do modal de edição
+
+  useEffect(() => {
+    const fetchPlaylist = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const decodedToken = jwtDecode(token);
+        const userId = decodedToken?.id;
+
+        const response = await axios.get(
+          `https://mqjnto3qw2.execute-api.us-east-1.amazonaws.com/default/playlist/${id}`,
+          { headers: { Authorization: token } }
+        );
+
+        const playlistData = response.data.playlist;
+        setPlaylist(playlistData);
+        setIsCreator(playlistData._userId === userId);
+        setNewPlaylistName(playlistData._name);  // Definindo o nome da playlist para edição
+
+        const musicDetailsPromises = playlistData._songs.map(async (songId) => {
+          try {
+            const songResponse = await axios.get(
+              `https://mqjnto3qw2.execute-api.us-east-1.amazonaws.com/default/song/${songId}`,
+              { headers: { Authorization: token } }
+            );
+            return songResponse.data.song;
+          } catch (err) {
+            console.error(`Erro ao carregar a música ${songId}:`, err);
+            return null;
+          }
+        });
+
+        const musicDetails = await Promise.all(musicDetailsPromises);
+        setMusic(musicDetails.filter((detail) => detail));
+      } catch (err) {
+        console.error("Erro ao carregar os detalhes da playlist:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPlaylist();
+  }, [id]);
+
+  const handleLeavePlaylist = () => {
+    if (location.pathname.includes("userPage")) {
+      navigate("/userPage");
+    } else {
+      navigate("/homePage");
+    }
+  };
+
+  const handleDeletePlaylist = async () => {
+    const confirmDelete = window.confirm("Tem certeza de que deseja excluir esta playlist?");
+    if (confirmDelete) {
+      try {
+        await deletePlaylist(id);
+        navigate("/userPage");
+      } catch (err) {
+        console.error("Erro ao deletar a playlist:", err);
+      }
+    }
+  };
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <GlobalStyled />
+      <Container>
+        {loading ? (
+          <SpinnerContainer>
+            <Spinner />
+          </SpinnerContainer>
+        ) : (
+          <>
+            <LeaveButton onClick={handleLeavePlaylist}>
+              <HiOutlineArrowSmLeft />
+            </LeaveButton>
+
+            <PlaylistHeader>
+              <PlaylistInfo>
+                {playlist?._songs[0] ? (
+                  <PlaylistImage
+                    src={`https://img.youtube.com/vi/${playlist?._songs[0]}/hqdefault.jpg`}
+                    alt={playlist?.name}
+                  />
+                ) : (
+                  <DefaultImage>Padrão</DefaultImage>
+                )}
+
+                <PlaylistDetails>
+                  <h1>{playlist?._name}</h1>
+                  <p>{playlist?._description}</p>
+                  {isCreator && (
+                    <div>
+                      <ButtonDelete onClick={handleDeletePlaylist}>Excluir Playlist</ButtonDelete>
+                      {/* Adicionando botão para editar a playlist */}
+                      <ButtonDelete onClick={handleOpenModal}>Editar Playlist</ButtonDelete>
+                    </div>
+                  )}
+                </PlaylistDetails>
+              </PlaylistInfo>
+            </PlaylistHeader>
+
+            <PlaylistTable>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Imagem</th>
+                  <th>Nome</th>
+                  <th>Artista</th>
+                  {isCreator && <th>Ações</th>}
+                </tr>
+              </thead>
+              <tbody>
+                {music.length === 0 ? (
+                  <tr>
+                    <td colSpan="4">Nenhuma música encontrada.</td>
+                  </tr>
+                ) : (
+                  music.map((song, index) => (
+                    <tr key={song.id}>
+                      <td>{index + 1}</td>
+                      <td>
+                        <Thumbnail
+                          src={`https://img.youtube.com/vi/${song.url.split("v=")[1]}/hqdefault.jpg`}
+                          alt={song.title}
+                        />
+                      </td>
+                      <td>{song.title}</td> {/* Nome da música */}
+                      <td>{song.artist}</td> {/* Nome do artista */}
+                      {isCreator && (
+                        <td>
+                          {/* Ações, se necessário */}
+                        </td>
+                      )}
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </PlaylistTable>
+          </>
+        )}
+      </Container>
+
+      
+{/* Modal de edição de playlist */}
+{isModalOpen && (
+  <EditPlaylistComponent
+    playlistId={id} // Passando o ID da playlist para o EditPlaylistComponent
+    initialPlaylistName={newPlaylistName}  // Passando o nome atual da playlist para edição
+    onClose={handleCloseModal}  // Função para fechar o modal
+  />
+)}
+    </>
+  );
+}
+
+export default PlaylistView;
---
+++ src/styles/Paginaprincipal/PlaylistViewStyled.jsx
@@ -0,0 +1,194 @@
+import styled from "styled-components";
+
+// Estilos gerais para o Container
+export const Container = styled.div`
+  padding: 20px 12%;
+  font-family: Arial, sans-serif;
+  margin-top: 4%;
+  
+  @media (max-width: 768px) {
+    padding: 10px 5%;
+  }
+`;
+
+// Header da playlist
+export const PlaylistHeader = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 40px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+// Informações da playlist
+export const PlaylistInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+// Imagem da playlist
+export const PlaylistImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 10px;
+  border: 2px solid chartreuse;
+`;
+
+// Detalhes da playlist
+export const PlaylistDetails = styled.div`
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    color: chartreuse;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 1rem;
+    color: #ffffff;
+  }
+
+  @media (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+// Botão para sair
+export const LeaveButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 1rem;
+  border-radius: 5px;
+  margin-bottom: 20px;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+// Tabela de músicas
+export const PlaylistTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+  background-color: #3a3838;
+  border-radius: 12px;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+
+  
+  td,th {
+    padding: 12px;
+    color: chartreuse;
+  }
+
+  th {
+    font-weight: bold;
+    font-size: 1.1rem;
+    border-bottom: 2px solid #444;
+    color: chartreuse;
+  }
+
+  td {
+    font-size: 1rem;
+    border-bottom: 1px solid #333;
+  }
+
+  tr:hover td {
+    background-color: #2a2a2a;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+  }
+`;
+
+// Thumbnail das músicas
+export const Thumbnail = styled.img`
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 5px;
+`;
+
+// Placeholder para a imagem padrão
+export const DefaultImage = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 1rem;
+  border-radius: 10px;
+`;
+
+// Spinner
+export const SpinnerContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+`;
+
+export const Spinner = styled.div`
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+// Botões de ação (Editar e Deletar)
+export const ButtonDelete = styled.button`
+  background-color: #ff4747;
+  color: white;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 20px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #e04040;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
