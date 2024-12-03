2dbda9a2a9d6d03b9d9d2ccd97d9350048cd25c5
HianMaths <hianmatheusofc1@gmail.com>
2024-12-02 23:55:18 -0300

    Atualização de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+
+
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import LandingPage from './pages/LandingPage';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Login from './pages/Login'
+import LandingPage from './pages/LandingPage'
+import Home from './pages/Home'
+import PlaylistView from './components/PaginaPrincipal/PlaylistView'
+import User from './pages/User'
 
 function App() {
   return (
@@ -11,6 +15,8 @@ function App() {
         <Route path="/" element={<LandingPage/>} />
         <Route path='/loginPage' element={<Login/>} />
         <Route path='/homePage' element={<Home/>}/>
+        <Route path="/playlist/:id" element={<PlaylistView/>} />
+        <Route path="/userPage" element={<User/>}/>
       </Routes>
     </Router>
     </>
@@ -18,3 +24,4 @@ function App() {
 }
 
 export default App
+
